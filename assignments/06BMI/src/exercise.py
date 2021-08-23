9f9f5c6b0b85e0e3e25fcf6604625dbfcba804e7
Daviddiaz22 <88851000+Daviddiaz22@users.noreply.github.com>
2021-08-23 11:17:32 -0500

    Update exercise.py

--- assignments/06BMI/src/exercise.py
+++ assignments/06BMI/src/exercise.py
@@ -5,22 +5,26 @@ def main():
 
     bmi = (peso / (altura**2))
 
-    if bmi < 20 and bmi>0:
-         print('PESO BAJO')
+    if peso < 0 or altura < 0:
+         print('Revisa tus datos, alguno de ellos es erróneo.')
     else:
-         if bmi<=0:
-              print('Revisa tus datos, alguno de ellos es erróneo.')
+         if bmi > 0 and bmi < 20:
+              print('PESO BAJO')
          else:
-              if 20 <= bmi < 25:
-                   print('NORMAL')
+              if bmi == 0 or bmi < 0:
+                   print('Revisa tus datos, alguno de ellos es erróneo.')
               else:
-                   if 25 <= bmi < 30:
-                        print('SOBREPESO')
+                   if 20 <= bmi < 25:
+                        print('NORMAL')
                    else:
-                        if 30 <= bmi < 40:
-                             print('OBESIDAD')
+                        if 25 <= bmi < 30:
+                             print('SOBREPESO')
                         else:
-                             if bmi >= 40:
-                                  print('OBESIDAD MORBIDA')
+                             if 30 <= bmi < 40:
+                                  print('OBESIDAD')
+                             else:
+                                  if bmi >= 40:
+                                       print('OBESIDAD MORBIDA')
+                    
 if __name__=='__main__':
     main()
