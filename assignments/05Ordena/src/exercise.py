61173e292d11d1934d1ff7e379160639b9b01a5c
Daviddiaz22 <88851000+Daviddiaz22@users.noreply.github.com>
2021-08-22 23:31:20 -0500

    Update exercise.py

--- assignments/05Ordena/src/exercise.py
+++ assignments/05Ordena/src/exercise.py
@@ -5,31 +5,31 @@ def main():
     num3 = int(input('Ingresa el tercer número: '))
 
     if num1 <= num2 and num1 <= num3:
-        print(num1)
+         print(num1)
     else:
-        if num2 <= num1 and num2 <= num3:
-            print(num2)
-        else:
-            if num3 <= num1 and num3<= num2:
-                print(num3)   
- 
+         if num2 <= num1 and num2 <= num3:
+              print(num2)
+         else:
+              if num3 <= num1 and num3<= num2:
+                   print(num3)   
+
     if num2 > num1 > num3 or num3 > num1 > num2 :
-        print(num1)
+         print(num1)
     else:
-        if num1 > num2 > num3 or num3 > num2 > num1:
-            print(num2)
-        else:
-            if num1 > num3 > num2 or num2 > num3 > num1:
-                print(num3)    
+         if num1 > num2 > num3 or num3 > num2 > num1:
+              print(num2)
+         else:
+              if num1 > num3 > num2 or num2 > num3 > num1:
+                   print(num3)    
 
     if num1 >= num2 and num1 >= num3:
-        print(num1)
+         print(num1)
     else:
-        if num2 >= num1 and num2 >= num3:
-            print(num2)
-        else:
-            if num3 >= num1 and num3>= num2:
-                print(num3)   
+         if num2 >= num1 and num2 >= num3:
+              print(num2)
+         else:
+              if num3 >= num1 and num3>= num2:
+                   print(num3)     
 
 
 if __name__=='__main__':
